72be497e7c5a6be38442f989ebe394f45121861a
DanilK <Fdfpor@yandex.ru>
2022-10-19 15:35:43 +0300

    used run build

---
+++ build/static/js/19.3b6fcb25.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpemilus=this.webpackJsonpemilus||[]).push([[19],{517:function(e,s,n){"use strict";n.r(s);var t=n(2);n(0);s.default=function(){return Object(t.jsx)("div",{children:"Home component works!"})}}}]);
+//# sourceMappingURL=19.3b6fcb25.chunk.js.map
